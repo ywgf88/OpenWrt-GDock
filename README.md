9242180f7e1395f23055010b74b8115b40e4ef7a
P3TERX <p3terx@outlook.com>
2019-12-20 10:39:36 +0800

    .

---
+++ README.md
@@ -0,0 +1,3 @@
+# OpenWrt firmware for Newifi D2
+
+[Download](https://github.com/P3TERX/OpenWrt-Newifi_D2/releases/latest)
---
+++ diy.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+#=================================================
+#   Description: DIY script
+#   Lisence: MIT
+#   Author: P3TERX
+#   Blog: https://p3terx.com
+#=================================================
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
