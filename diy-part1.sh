fc9b0269ae90bac169e69602bd5537c62468e622
ywgf88 <43352863+ywgf88@users.noreply.github.com>
2020-09-29 20:45:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,3 +10,18 @@
 
 # fw876/helloworld
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+
+# Add a feed source
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+
+#Clone Lienol source code
+git clone https://github.com/kenzok8/openwrt-packages.git  package/diy-packages
+git clone https://github.com/kenzok8/small.git  package/openwrt-small
+
+#添加jerrykuku
+sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+
+# 删除默认argon主题，并下载新argon主题
+rm -rf ./package/lean/luci-theme-argon
